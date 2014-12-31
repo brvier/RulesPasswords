b8622c5b9a241c0f1c6dade9994ab44b7d605fd3
Benoît HERVIER <khertan@khertan.net>
2014-12-31 11:27:44 +0100

    Fixing alert for copy, cleaning repository

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,17 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.verso.cordova.clipboard/www/clipboard.js",
-        "id": "com.verso.cordova.clipboard.Clipboard",
-        "clobbers": [
-            "cordova.plugins.clipboard"
-        ]
-    },
-    {
         "file": "plugins/com.blackberry.community.clipboard/www/client.js",
         "id": "com.blackberry.community.clipboard.client",
         "clobbers": [
             "community.clipboard"
+        ]
+    },
+    {
+        "file": "plugins/com.verso.cordova.clipboard/www/clipboard.js",
+        "id": "com.verso.cordova.clipboard.Clipboard",
+        "clobbers": [
+            "cordova.plugins.clipboard"
         ]
     },
     {
@@ -30,8 +30,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.verso.cordova.clipboard": "0.1.0",
     "com.blackberry.community.clipboard": "1.0.0",
+    "com.verso.cordova.clipboard": "0.1.0",
     "org.apache.cordova.device": "0.2.13"
 }
 // BOTTOM OF METADATA
