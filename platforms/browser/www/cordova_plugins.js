3d4378c1d3bc1a05a11947dd9567d6c9ea1ea076
Benoît <khertan@khertan.net>
2015-08-28 08:07:30 +0200

    Add missing file in repository for release, and cleaning temp file out of repository

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.blackberry.community.clipboard/www/client.js",
-        "id": "com.blackberry.community.clipboard.client",
-        "clobbers": [
-            "community.clipboard"
-        ]
-    },
-    {
         "file": "plugins/com.verso.cordova.clipboard/www/clipboard.js",
         "id": "com.verso.cordova.clipboard.Clipboard",
         "clobbers": [
@@ -30,7 +23,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.blackberry.community.clipboard": "1.0.0",
     "com.verso.cordova.clipboard": "0.1.0",
     "org.apache.cordova.device": "0.2.13"
 }
