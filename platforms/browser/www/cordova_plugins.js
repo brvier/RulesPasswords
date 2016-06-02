a4f0d870e102493e971a205284a0240fa371231d
Benoît HERVIER <khertan@khertan.net>
2016-06-02 18:32:29 +0200

    v0.3.0 : use crosswalk, add webintent

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -3,6 +3,7 @@ module.exports = [
     {
         "file": "plugins/com.verso.cordova.clipboard/www/clipboard.js",
         "id": "com.verso.cordova.clipboard.Clipboard",
+        "pluginId": "com.verso.cordova.clipboard",
         "clobbers": [
             "cordova.plugins.clipboard"
         ]
@@ -10,6 +11,7 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
+        "pluginId": "org.apache.cordova.device",
         "clobbers": [
             "device"
         ]
@@ -17,6 +19,7 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.device/src/browser/DeviceProxy.js",
         "id": "org.apache.cordova.device.DeviceProxy",
+        "pluginId": "org.apache.cordova.device",
         "runs": true
     }
 ];
@@ -24,7 +27,9 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "com.verso.cordova.clipboard": "0.1.0",
-    "org.apache.cordova.device": "0.2.13"
+    "org.apache.cordova.device": "0.2.13",
+    "com.napolitano.cordova.plugin.intent": "0.1.2",
+    "cordova-plugin-crosswalk-webview": "1.7.2"
 }
 // BOTTOM OF METADATA
 });
